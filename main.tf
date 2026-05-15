8e3f2c9f1217ad51b8eeff310ca84ff5c01f8cc3
Chandu Vijayaraj <chavijnam@gmail.com>
2026-05-15 15:27:27 +0100

    feat: add key vault and static secret

--- main.tf
+++ main.tf
@@ -5,29 +5,43 @@ terraform {
       version = "~> 3.0"
     }
   }
-  
-  # This tells Terraform to look in Azure for its memory, not the local disk
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state-demo"
-    storage_account_name = "tfstatedemostorage001" # Update if you changed this
+    storage_account_name = "tfstatedemostorage2026" # Use your unique name
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
 }
 
-# The actual infrastructure we want to build
-resource "random_id" "storage_id" {
+# 1. Fetch the Resource Group
+data "azurerm_resource_group" "demo" {
+  name = "rg-cloud-access-demo"
+}
+
+# 2. Create the Key Vault
+resource "azurerm_key_vault" "vault" {
+  name                = "kv-demo-${random_id.vault_id.hex}"
+  location            = data.azurerm_resource_group.demo.location
+  resource_group_name = data.azurerm_resource_group.demo.name
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
+}
+
+resource "random_id" "vault_id" {
   byte_length = 4
 }
 
-resource "azurerm_storage_account" "demo" {
-  name                     = "prodstorage${random_id.storage_id.hex}"
-  resource_group_name      = "rg-cloud-access-demo"
-  location                 = "uksouth"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# 3. Add a secret to the vault (The "Secret Trap")
+resource "azurerm_key_vault_secret" "example" {
+  name         = "database-password"
+  value        = "InitialStaticPassword123!"
+  key_vault_id = azurerm_key_vault.vault.id
 }
